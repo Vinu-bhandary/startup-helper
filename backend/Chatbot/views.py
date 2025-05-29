fda38269b6682beb3f42f388944eaa90c3fb4b6a
Vinaychandra R Bhandary <147505476+Vinu-bhandary@users.noreply.github.com>
2025-05-29 15:04:16 +0530

    Update views.py

--- backend/Chatbot/views.py
+++ backend/Chatbot/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCcfpReTAV2uYBvxdoX37q8vGkPsaZiGjQ")
+genai.configure(api_key="")
 
 model = genai.GenerativeModel("gemini-2.0-flash")
 def chat_with_gemini(user_input):
